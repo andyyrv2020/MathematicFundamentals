285a6daa45a4d4ba0cd223aa76724e544ee9e28c
PC-23 <it.pgee.burgas@gmail.com>
2024-06-03 11:26:02 +0300

    Add project files.

---
+++ MathematicFundamentals/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MathematicFundamentals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
