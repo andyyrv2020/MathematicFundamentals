4605f0b04d85c62ede4ebed571803354d3a75a3a
PC-23 <it.pgee.burgas@gmail.com>
2024-06-03 11:27:27 +0300

    sad

---
+++ CombinatoricsOperations/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CombinatoricsOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
